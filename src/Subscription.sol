30d9d53698dad2b1cbf17a3f44395f67d16b0324
Sam Sends <sbsends@gmail.com>
2023-09-14 16:36:38 -0700

    ✨  added subscription liquidations

--- src/Subscription.sol
+++ src/Subscription.sol
@@ -19,10 +19,28 @@ contract Subscription {
         uint256 newAmountPerBlock
     );
     event SubscriptionCanceled(address indexed user, address indexed recipient);
+    event SubscriptionLiquidation(
+        address indexed user,
+        address indexed liquidator,
+        uint256 reward
+    );
 
     function depositETH() external payable {
         updateBalance(msg.sender);
         balances[msg.sender] += msg.value;
+    }
+
+    // New function for withdrawing funds
+    function withdraw(uint256 amount) external {
+        uint256 userOutflow = outflowRates[msg.sender];
+        uint256 liquidationThreshold = userOutflow * 6000; // 2 times the original threshold
+        require(
+            balances[msg.sender] >= liquidationThreshold,
+            "Below liquidation threshold"
+        );
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
     }
 
     function createSubscription(
@@ -46,8 +64,7 @@ contract Subscription {
 
     function updateSubscription(
         address recipient,
-        uint256 newAmountPerBlock,
-        uint256 oldAmountPerBlock
+        uint256 newAmountPerBlock
     ) external {
         require(
             newAmountPerBlock <= balances[msg.sender],
@@ -56,6 +73,8 @@ contract Subscription {
 
         updateBalance(msg.sender);
         updateBalance(recipient);
+
+        uint256 oldAmountPerBlock = subscriptions[msg.sender][recipient];
 
         outflowRates[msg.sender] =
             outflowRates[msg.sender] -
@@ -70,12 +89,11 @@ contract Subscription {
         emit SubscriptionUpdated(msg.sender, recipient, newAmountPerBlock);
     }
 
-    function cancelSubscription(
-        address recipient,
-        uint256 oldAmountPerBlock
-    ) external {
+    function cancelSubscription(address recipient) external {
         updateBalance(msg.sender);
         updateBalance(recipient);
+
+        uint256 oldAmountPerBlock = subscriptions[msg.sender][recipient];
 
         outflowRates[msg.sender] -= oldAmountPerBlock;
         inflowRates[recipient] -= oldAmountPerBlock;
@@ -118,5 +136,51 @@ contract Subscription {
         uint256 currentBalance = getBalance(user);
         uint256 currentRate = subscriptions[user][recipient];
         return currentBalance >= currentRate && currentRate == expectedRate;
+    }
+
+    function liquidateSubscriptions(
+        address user,
+        address[] memory recipients
+    ) external {
+        uint256 userBalance = balances[user]; // Single read
+        uint256 userOutflow = outflowRates[user]; // Single read
+        uint256 liquidationThreshold = userOutflow * 3000;
+
+        // Check if the user's balance is below the liquidation threshold
+        if (userBalance < liquidationThreshold) {
+            uint256 totalCanceledOutflow = 0;
+
+            // Cancel all subscriptions for this user
+            for (uint i = 0; i < recipients.length; i++) {
+                address recipient = recipients[i];
+                uint256 oldAmountPerBlock = subscriptions[user][recipient];
+                userOutflow -= oldAmountPerBlock; // Update in memory
+                inflowRates[recipient] -= oldAmountPerBlock; // Single write per loop iteration
+                subscriptions[user][recipient] = 0; // Single write per loop iteration
+                totalCanceledOutflow += oldAmountPerBlock;
+                emit SubscriptionCanceled(user, recipient);
+            }
+
+            // Calculate the reward for the liquidator
+            uint256 remainingBlocks = userBalance / userOutflow;
+            uint256 reward = totalCanceledOutflow * (remainingBlocks / 2);
+
+            // If outflow is zero, give the liquidator all remaining user funds
+            if (userOutflow == 0) {
+                reward = userBalance;
+            }
+
+            // Cap the reward to the user's remaining balance
+            reward = (reward > userBalance) ? userBalance : reward;
+
+            // Update balances and outflowRates in storage (single write)
+            balances[user] = userBalance - reward;
+            balances[msg.sender] += reward;
+            outflowRates[user] = userOutflow;
+
+            // Emit an event to notify that the user's subscriptions have been liquidated
+            emit SubscriptionLiquidation(user, msg.sender, reward);
+            (user, msg.sender, reward);
+        }
     }
 }
